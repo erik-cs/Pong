6c78993340c8ffaa44bbcda40f22482821ce4257
Erik Cutsor <eacutsor@gmail.com>
2016-05-14 12:13:36 -0700

    Pong. Boing. Boing.

---
+++ game.js
@@ -0,0 +1,7 @@
+var flop = [0, 1, 2, 3];
+var playerFlopStraightFlush = [];
+
+Array.prototype.push.apply(playerFlopStraightFlush, flop);
+
+console.log(flop);
+console.log(playerFlopStraightFlush);
